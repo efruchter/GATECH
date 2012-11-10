2c4f82ce910a818e3c6ef596199875afe5bdc42c
Chad <chad@chad-Studio-1458.(none)>
2012-11-10 18:36:30 -0500

    Parsing stuff is coming together - what!??

--- sub/src/spec/TokenDef.java
+++ sub/src/spec/TokenDef.java
@@ -1,16 +1,34 @@
 package spec;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 public class TokenDef {
-    private final String tokenName;
-    private final String tokenStuff;
+	private final String tokenName;
+	private String tokenStuff;
 
-    public TokenDef(final String tokenName, final String tokenStuff) {
-        this.tokenName = tokenName;
-        this.tokenStuff = tokenStuff;
-    }
+	public TokenDef(final String tokenName, final String tokenStuff,
+			final Map<String, CharClass> charClasses) {
+		this.tokenName = tokenName;
+		this.tokenStuff = tokenStuff;
+		collapse(charClasses);
+	}
 
-    @Override
-    public String toString() {
-        return String.format("<TokenDef $%s %s>", this.tokenName, this.tokenStuff);
-    }
+	private void collapse(Map<String, CharClass> charClasses) {
+		// TODO Auto-generated method stub
+		Iterator charClassesIterator = charClasses.entrySet().iterator();
+		while (charClassesIterator.hasNext()) {
+			Map.Entry<String, CharClass> entry = (Map.Entry) charClassesIterator
+					.next();
+			this.tokenStuff = this.tokenStuff.replace("$" + entry.getKey(), entry.getValue().getRe());
+		}
+		this.tokenStuff = CharClass.parseFinal(this.tokenStuff);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("<TokenDef $%s %s>", this.tokenName,
+				this.tokenStuff);
+	}
 }
