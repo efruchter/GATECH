9fe3cd0e7022bfafe0c1590b90844c01def7a26e
Chad <chad@chad-Studio-1458.(none)>
2012-11-10 22:03:00 -0500

    Changes and stuff (working parser).

--- sub/src/scannergenerator/ScannerGeneratorTest.java
+++ sub/src/scannergenerator/ScannerGeneratorTest.java
@@ -15,24 +15,27 @@ public class ScannerGeneratorTest {
 
 		RDPControl rdpc = new RDPControl();
 
-		tests.add("[a-z]");
+		tests.add("[A-C^D-V]IN[A-Z]");
 		tests.add("[a-zA-Z]");
 		tests.add("[a-zA-Z0-9]");
 		tests.add("[a-zA-Z0-9a-zA-Z0-9]");
 		tests.add("[^0]IN[0-9]");
 		tests.add("[^G]IN[A-Z]");
 		tests.add("[^A-C]IN[A-L]");
-		tests.add("a+ttttt");
+		tests.add("a+(b[a-z])+(cde)+[a-z]+");
 		tests.add("[^a-z]IN[A-Za-z]([A-Za-z]|[0-9])*");
 		tests.add("([A-Za-z]|[0-9])*");
 		tests.add("[^a-z]IN[A-Za-z]*");
+		tests.add("(a+)+");
 
+		System.out.println(rdpc.myParser("[a-z]"));
 		System.out.println("Testing");
 		for (String st : tests) {
 			System.out.println("Test " + st + ": " + rdpc.parseFinal(st));
 		}
 	}
 	/*
+<<<<<<< Updated upstream
 	 * @Test public void apiTest() { List<DefinedClass> dc =
 	 * RDPControl.getOutput(""); for(DefinedClass c : dc) {
 	 * System.out.printf("%s %s\n", c.getName(), String.valueOf(c.getRegex()));
@@ -43,4 +46,11 @@ public class ScannerGeneratorTest {
 	 * String.valueOf(c.getRegex())); } }
 	 */
 
+=======
+	 * @Test public void apiTest() { DefinedClass[] dc =
+	 * RDPControl.getOutput(""); for (DefinedClass c : dc) {
+	 * System.out.printf("%s %s\n", c.getName(), String.valueOf(c.getRegex()));
+	 * } }
+	 */
+>>>>>>> Stashed changes
 }
