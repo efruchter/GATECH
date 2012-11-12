79c0935c3095ebf1c2f246265badeef85dea1bc4
Patrick Lucas <me@patricklucas.com>
2012-11-12 15:28:12 -0500

    Big cleanup of Tokenizer

--- sub/src/tokenizer/Tokenizer.java
+++ sub/src/tokenizer/Tokenizer.java
@@ -14,14 +14,20 @@ import java.util.Iterator;
  *
  */
 public class Tokenizer implements Iterable<Token> {
-	private BufferedReader br;
-	private StringBuilder stackBuffer = new StringBuilder();
-	private StringBuilder lineBuffer = new StringBuilder();
-	private NFA dfa;
+	private final BufferedReader reader;
+	private final NFA dfa;
+	private String curLine;
+	private Token nextToken = null;
+
+	public Tokenizer(final NFA dfa, final InputStream input) {
+		if (!dfa.isDFA())
+			throw new RuntimeException("Must be DFA");
+
+		this.dfa = dfa;
+		this.reader = new BufferedReader(new InputStreamReader(input));
+	}
 
 	private class TokenIterator implements Iterator<Token> {
-		private Token nextToken = null;
-
 		public boolean hasNext() {
 			if (nextToken == null) {
 				nextToken = getNextToken();
@@ -31,13 +37,7 @@ public class Tokenizer implements Iterable<Token> {
 		}
 
 		public Token next() {
-			if (nextToken != null) {
-				Token token = nextToken;
-				nextToken = null;
-				return token;
-			} else {
-				return getNextToken();
-			}
+			return getNextToken();
 		}
 
 		public void remove() {
@@ -45,89 +45,70 @@ public class Tokenizer implements Iterable<Token> {
 		}
 	}
 
-	/**
-	 * Tokenizer constructor
-	 * 
-	 * @param dfa
-	 * @param input
-	 */
-	public Tokenizer(final NFA dfa, final InputStream input) {
-		if (!dfa.isDFA())
-			throw new RuntimeException("nfa is not a dfa for tokenizer");
-
-		this.dfa = dfa;
-		this.br = new BufferedReader(new InputStreamReader(input));
-	}
-
 	public Iterator<Token> iterator() {
 		return new TokenIterator();
 	}
-	
-	/**
-	 * Append next line from stream to lineBuffer
-	 * 
-	 * @return true if non-null line is added
-	 */
-	private boolean bufferLine() {
-		try {
-			String currLine = br.readLine();
-			if (currLine != null) {
-				lineBuffer.append(currLine);
-				return true;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
 
-	/**
-	 * Gets the next token
-	 *
-	 * @param nfa
-	 *			the nfa to check the string against
-	 * @param string
-	 *			string to check for validity
-	 * @return true if the string is valid, false otherwise.
-	 */
 	public Token getNextToken() {
-		//stream is exhauseted, return null
-		if(lineBuffer.length() == 0 && !bufferLine()) {
-				return null;
+		if (nextToken != null) {
+			Token token = nextToken;
+			nextToken = null;
+			return token;
 		}
-		
+
+		if (curLine == null || curLine.length() == 0) {
+			try {
+				curLine = reader.readLine();
+			} catch (IOException ex) {
+				return null;
+			}
+
+			// If curLine is still null, no more input
+			if (curLine == null) {
+				return null;
+			}
+		}
+
 		Token t = null;
-		for (int max = lineBuffer.length(); max > 0; max--) {
+
+		for (int max = curLine.length(); max > 0; max--) {
 			t = getNextToken(dfa.getStartState(), 0, max);
 			if (t != null) {
-				lineBuffer.delete(0, t.value.length());
+				curLine = curLine.substring(t.value.length());
 				break;
 			}
 		}
-		if (t == null && lineBuffer.length() > 0) {
-			lineBuffer.deleteCharAt(0);
+
+		if (t == null && curLine.length() > 0) {
+			curLine = curLine.substring(1);
 			return getNextToken();
 		}
+
 		return t;
 	}
 
 	private Token getNextToken(State state, int min, int max) {
+		return getNextToken(state, min, max, new StringBuffer());
+	}
+
+	private Token getNextToken(State state, int min, int max, StringBuffer tokenBuffer) {
 		Token t = null;
-		//find transition if any
+
 		for (Transition tr : state.getTransitions()) {
 			if (min == max) {
 				break;
 			}
-			if (tr.isValid(String.valueOf(lineBuffer.charAt(min)))) {
-				stackBuffer.append(lineBuffer.charAt(min));
-				return getNextToken(tr.getDestinationState(), min + 1, max);
+			String c = String.valueOf(curLine.charAt(min));
+			if (tr.isValid(c)) {
+				tokenBuffer.append(c);
+				return getNextToken(tr.getDestinationState(), min + 1, max, tokenBuffer);
 			}
 		}
+
 		if (state.isFinal()) {
-			t = new Token(state.getName(), stackBuffer.toString());
+			t = new Token(state.getName(), tokenBuffer.toString());
 		}
 		
-		stackBuffer.setLength(0);
 		return t;
 	}
 }
