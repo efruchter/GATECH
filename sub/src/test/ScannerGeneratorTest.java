6c96289a648219ed81075408817a485112e71d22
Patrick Lucas <me@patricklucas.com>
2012-11-12 14:38:31 -0500

    Improve (and make work) ScannerGeneratorTest

--- sub/src/test/ScannerGeneratorTest.java
+++ sub/src/test/ScannerGeneratorTest.java
@@ -9,21 +9,45 @@ import tokenizer.Tokenizer;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class ScannerGeneratorTest {
 
-    private final String simpleSpec = "\n\n$INT 0";
-    private final String simpleInput = "\n\n$INT 0";
-    private final String simpleOutput = "\n\n$INT 0";
-    private final String complexSpec = "\n\n$INT 0";
-    private final String complexInput = "\n\n$INT 0";
-    private final String complexOutput = "\n\n$INT 0";
+    private class ScannerGeneratorTestCase {
+        public final String spec, input, output;
 
-    private void runScannerGeneratorTest(final String spec, final String input, final String output) {
-        InputStream specInputStream = new ByteArrayInputStream(spec.getBytes());
-        InputStream inputInputStream = new ByteArrayInputStream(input.getBytes());
+        public ScannerGeneratorTestCase(final String spec, final String input, final String output) {
+            this.spec = spec;
+            this.input = input;
+            this.output = output;
+        }
+    }
+
+    private final List<ScannerGeneratorTestCase> testCases = new ArrayList<ScannerGeneratorTestCase>() {{
+        add(new ScannerGeneratorTestCase(
+                "\n$DIGIT 0",
+                "000",
+                "DIGIT 0\nDIGIT 0\nDIGIT 0\n"
+        ));
+        add(new ScannerGeneratorTestCase(
+                "$DIGIT [0-3]\n$CHAR [a-c]\n\n$INT $DIGIT+\n$WORD $CHAR+",
+                "abc 12 3cba0",
+                "WORD abc\nINT 12\nINT 3\nWORD cba\nINT 0\n"
+        ));
+        add(new ScannerGeneratorTestCase(
+                "$DIGIT [0-9]\n$NON-ZERO [^0] IN $DIGIT\n$CHAR [a-z]\n\n" +
+                        "$INT $NON-ZERO $DIGIT*\n$VAR $CHAR ($CHAR | $DIGIT)*",
+                "123 0123 6abc abc67",
+                "INT 123\nINT 123\nINT 6\nVAR abc\nVAR abc67\n"
+        ));
+    }};
+
+    private void runScannerGeneratorTest(ScannerGeneratorTestCase testCase) {
+        InputStream specInputStream = new ByteArrayInputStream(testCase.spec.getBytes());
+        InputStream inputInputStream = new ByteArrayInputStream(testCase.input.getBytes());
         ScannerGenerator scannerGenerator = new ScannerGenerator(specInputStream, inputInputStream);
         Tokenizer tokenizer = scannerGenerator.generateTokenizer();
 
@@ -34,12 +58,13 @@ public class ScannerGeneratorTest {
             sb.append(token.toString()).append("\n");
         }
 
-        assertEquals(output, sb.toString());
+        assertEquals(testCase.output, sb.toString());
     }
 
     @Test
     public void testScannerGenerator() {
-        runScannerGeneratorTest(simpleSpec, simpleInput, simpleOutput);
-        runScannerGeneratorTest(complexSpec, complexInput, complexOutput);
+        for (ScannerGeneratorTestCase testCase : testCases) {
+            runScannerGeneratorTest(testCase);
+        }
     }
 }
