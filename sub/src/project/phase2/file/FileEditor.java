fe14917b893028ee519160f72b1651035dd73b9b
efruchter <fruchter.eric@gmail.com>
2012-12-08 14:02:43 -0500

    fix for readfile

--- sub/src/project/phase2/file/FileEditor.java
+++ sub/src/project/phase2/file/FileEditor.java
@@ -1,44 +1,51 @@
 package project.phase2.file;
 
+import project.phase2.structs.StringMatchList;
+import project.phase2.structs.StringMatchTuple;
+
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * File editing tool.
  */
 public class FileEditor {
 
-    private FileEditor() {}
+    private FileEditor() {
+    }
 
     /**
      * Replace a substring in a file with another string.
-     * @param file the file
-     * @param replaceMe the string to replace
+     *
+     * @param file        the file
+     * @param replaceMe   the string to replace
      * @param replaceWith the string to replace replaceMe with
      * @throws IOException file writing/reading has been blocked
      */
-    public static void replaceAllSubstring (final File file, final String replaceMe, final String replaceWith) throws IOException {
+    public static void replaceAllSubstring(final File file, final String replaceMe, final String replaceWith) throws IOException {
         String print = readEntireFile(file).replaceAll(replaceMe, replaceWith);
         BufferedWriter out = new BufferedWriter(new FileWriter(file));
         out.write(print);
         out.close();
     }
 
+    public static StringMatchTuple findInFile(final String s) {
+        return null;
+    }
+
     /**
      * Convert an entire file to a string.
+     *
      * @param file the file
      * @return the string representing the file
      * @throws IOException file reading has been blocked
      */
     public static String readEntireFile(final File file) throws IOException {
-        FileReader in = new FileReader(file);
-        StringBuilder contents = new StringBuilder();
-        char[] buffer = new char[4096];
-        int read = 0;
-        do {
-            contents.append(buffer, 0, read);
-            read = in.read(buffer);
-        } while (read >= 0);
-        return contents.toString();
+        StringBuffer a = new StringBuffer();
+        Scanner s = new Scanner(file);
+        while (s.hasNextLine()) {
+            a.append("\n").append(s.nextLine());
+        }
+        return a.toString().replaceFirst("\n", "");
     }
-
 }
--- sub/src/project/phase2/structs/StringMatchList.java
+++ sub/src/project/phase2/structs/StringMatchList.java
@@ -2,7 +2,7 @@ package project.phase2.structs;
 
 import java.util.ArrayList;
 
-public class StringMatchList extends ArrayList<StringMatchList.StringMatchTuple> {
+public class StringMatchList extends ArrayList<StringMatchTuple> {
 
     public StringMatchList() {
         // Default
@@ -106,7 +106,6 @@ public class StringMatchList extends ArrayList<StringMatchList.StringMatchTuple>
             }
         }
 
-
         return true;
     }
 
@@ -119,46 +118,5 @@ public class StringMatchList extends ArrayList<StringMatchList.StringMatchTuple>
         }
         be.append("]");
         return be.toString().replaceFirst(", ", "");
-    }
-
-    public static class StringMatchTuple {
-
-        public String string;
-        public String fileName = "";
-        public int line = 0, startIndex = 0, endIndex = 0;
-
-        public StringMatchTuple() {
-            //default
-        }
-
-        public StringMatchTuple(String s) {
-            string = s;
-        }
-
-        public StringMatchTuple(StringMatchTuple s) {
-            string = s.string;
-            fileName = s.fileName;
-            line = s.line;
-            startIndex = s.startIndex;
-            endIndex = s.endIndex;
-        }
-
-        public String toString() {
-            // "cba"<"file1.txt", 40, 50>}
-            return "\"" + string + "\" <\"" + fileName + "\", " + line + ", " + startIndex + ", " + endIndex + ">";
-        }
-
-        @Override
-        public boolean equals(Object o) {
-
-            if (this == o)
-                return true;
-
-            if (!(o instanceof StringMatchTuple)) {
-                return false;
-            }
-
-            return string.equals(((StringMatchTuple) o).string);
-        }
     }
 }
